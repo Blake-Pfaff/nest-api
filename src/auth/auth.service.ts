1299fff79474af4cb4eec1f5c1ab7df2ee2838e6
Blake Pfaff <blake.pfaff@sgkinc.com>
2022-08-05 08:00:38 -0500

    changea

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { User, BookMark } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable({})
 export class AuthService {
+  constructor(private prisma: PrismaService) {}
   signUp() {
     return { message: 'I have signed up' };
   }
