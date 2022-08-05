92466797250fea667fa39c73cb10d98c1a50188b
Blake Pfaff <blake.pfaff@sgkinc.com>
2022-08-05 08:06:26 -0500

    add ground work

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User, BookMark } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
-@Injectable({})
+@Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   signUp() {
