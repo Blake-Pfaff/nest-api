c6789f41627d2944f2c68d4624c3607d0ee6569b
Blake Pfaff <blake.pfaff@sgkinc.com>
2022-08-06 14:41:28 -0500

    add tests and test logic for test DB

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,63 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
+import { AuthDto } from 'src/auth/dto';
 
-describe('AppController (e2e)', () => {
+describe('App e2e', () => {
   let app: INestApplication;
-
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  let prisma: PrismaService;
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
     await app.init();
-  });
+    await app.listen(3333);
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
   });
+  afterAll(() => {
+    app.close();
+  });
+  it.todo('should pass');
+});
+
+describe('Auth', () => {
+  describe('SignUp', () => {
+    it('Should sign up', () => {
+      const dto: AuthDto = {
+        email: 'bpfaff@gmail.com',
+        password: 'testPassword',
+      };
+      return pactum
+        .spec()
+        .post('http://localhost:3333/auth/signup')
+        .withBody(dto)
+        .expectStatus(201);
+    });
+  });
+  describe('SignIn', () => {
+    it.todo('Should sign in');
+  });
+});
+
+describe('User', () => {
+  describe('Get current user', () => {});
+  describe('Edit current user', () => {});
+});
+
+describe('BookMarks', () => {
+  describe('Create bookmark', () => {});
+  describe('Get bookmarks', () => {});
+  describe('Get bookmark by id', () => {});
+  describe('Edit bookmark by id', () => {});
+  describe('Delete bookmark by id', () => {});
 });
